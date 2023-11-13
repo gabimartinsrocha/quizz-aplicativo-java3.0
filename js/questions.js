77500942f5fcfb6ea6ff1da87e9435332efb7a69
gabs.martins <132924573+gabimartinsrocha@users.noreply.github.com>
2023-11-13 11:51:33 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,58 +2,58 @@
 let questions = [
   {
     numb: 1,
-    question: "Indique a alternativa em que o trecho destacado corresponde a uma oração subordinada adjetiva."
-    answer: "  Os professores que interagem motivam os alunos. ",
+    question: "Indique a alternativa em que o trecho destacado corresponde a uma oração subordinada adjetiva"
+    answer: "Os professores que interagem motivam os alunos",
     options: [
-      " Os alunos ficarão motivados desde que as aulas sejam interativas.",
-      " Os alunos querem uma coisa: que as aulas sejam interativas. ",
-      " Os professores que interagem motivam os alunos.",
-      " Os alunos querem que as aulas sejam interativas."
+      "Os alunos ficarão motivados desde que as aulas sejam interativas",
+      "Os alunos querem uma coisa: que as aulas sejam interativas",
+      "Os professores que interagem motivam os alunos",
+      "Os alunos querem que as aulas sejam interativas"
     ]
   },
   {
     numb: 2,
-    question: "Indique qual das alternativas contém uma oração subordinada adverbial temporal.",
+    question: "Indique qual das alternativas contém uma oração subordinada adverbial temporal",
    
-      answer: " Mal chegamos ao cinema, o filme começou. ",
+      answer: "Mal chegamos ao cinema, o filme começou",
     options: [
-      "  Mal chegamos ao cinema, o filme começou.  ",
-      " É provável que a gente se atrase.",
-      " As pessoas que chegam atrasadas atrapalham os outros.",
-      "Já que estamos atrasados, assistiremos a próxima sessão."
+      "Mal chegamos ao cinema, o filme começou",
+      "É provável que a gente se atrase",
+      "As pessoas que chegam atrasadas atrapalham os outros",
+      "Já que estamos atrasados, assistiremos a próxima sessão"
     ]
   },
   {
     numb: 3,
-    question: "Indique a alternativa que contém uma oração subordinada substantiva.",
-    answer: "A verdade é que somos super inteligentes!",
+    question: "Indique a alternativa que contém uma oração subordinada substantiva",
+    answer: "A verdade é que somos super inteligentes",
     options: [
       "Nós, que somos super inteligentes, estamos entendendo tudo",
-      "A verdade é que somos super inteligentes!",
-      "Nós, que estudamos muito, nos sentimos mais inteligentes.",
-      " Acho que nós somos mais inteligentes do que eles."
+      "A verdade é que somos super inteligentes",
+      "Nós, que estudamos muito, nos sentimos mais inteligentes",
+      "Acho que nós somos mais inteligentes do que eles"
     ]
   },
   {
     numb: 4,
     question: "Identifique o aposto especificativo na seguinte oração: "A tragédia Hamlet é uma das mais encenadas no mundo",
-    answer: " Hamlet",
+    answer: "Hamlet",
     options: [
-      " A tragédia",
-      " é uma das mais encenadas",
-      " no mundo",
-      " Hamlet"
+      "A tragédia",
+      "é uma das mais encenadas",
+      "no mundo",
+      "Hamlet"
     ]
   },
   {
     numb: 5,
-    question: "Assinale a alternativa que apresenta corretamente um aposto especificativo:",
-    answer: "Documentos, cartas e fotografias, tudo foi destruído pelo fogo.",
+    question: "Assinale a alternativa que apresenta corretamente um aposto especificativo",
+    answer: "Documentos, cartas e fotografias, tudo foi destruído pelo fogo",
     options: [
-      "Documentos, cartas e fotografias, tudo foi destruído pelo fogo.",
+      "Documentos, cartas e fotografias, tudo foi destruído pelo fogo",
       "Quero apenas duas coisas: saúde e paz",
-      " O aluno João deve comparecer ao ensaio amanhã.",
-      "Crianças, jovens e adultos, todos viram e não disseram nada."
+      "O aluno João deve comparecer ao ensaio amanhã",
+      "Crianças, jovens e adultos, todos viram e não disseram nada"
     ]
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
@@ -62,12 +62,12 @@ let questions = [
   //   {
   //   numb: 6,
   //   question: "Indique a única alternativa que contém aposto",
-  //   answer: "Aquilo exigia um esforço adicional, coisa que não queria fazer.",
+  //   answer: "Aquilo exigia um esforço adicional, coisa que não queria fazer",
   //   options: [
-  //     "Aquilo exigia um esforço adicional, coisa que não queria fazer.",
+  //     "Aquilo exigia um esforço adicional, coisa que não queria fazer",
   //     "A refeição, leve e saborosa, agradou aos convidados",
-  //     "Maria, que é a mais falante, não abriu a boca.",
-  //     " Esforçada, a equipe conquistou um lugar no pódio."
+  //     "Maria, que é a mais falante, não abriu a boca",
+  //     "Esforçada, a equipe conquistou um lugar no pódio"
   //   ]
   // },
 ];
